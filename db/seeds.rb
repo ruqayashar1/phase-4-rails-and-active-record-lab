1394f44948b9f48a372a08bcf4a193d3163d08d0
rukia <ruqayashar1@gmail.com>
2023-06-19 22:34:23 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+student1 = Student.create!(first_name: 'Michael', last_name: 'Johnson', grade: 9)
+student2 = Student.create!(first_name: 'Emily', last_name: 'Brown', grade: 12)
+puts student1.to_s
+puts student2.to_s
